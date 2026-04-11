4c86ae4c78e3105765ac662523428abe6f1321e4
Nazarii <gorchinshiy83@gmail.com>
2026-04-11 18:33:55 +0300

    feat: create grid renderer

---
+++ src/app/features/pixel-editor/engine/grid.ts
@@ -0,0 +1,57 @@
+import { Viewport } from '../../../core/models/viewport.model';
+import { Size } from '../../../core/models/canvas.model';
+
+export interface GridConfig {
+  readonly color: string;
+  readonly width: number;
+  readonly size: number;
+  readonly isEnable: boolean;
+  readonly minZoom: number;
+  readonly opacity: number;
+}
+
+export const DEFAULT_GRID_CONFIG: GridConfig = {
+    color: '#000000',
+    isEnable: false,
+    width: 1,
+    size: 8,
+    minZoom: 0.5,
+    opacity: 1,
+  }
+export class Grid {
+  config = {...DEFAULT_GRID_CONFIG};
+
+  public render(
+    ctx: CanvasRenderingContext2D,
+    viewport: Viewport,
+    canvasSize: Size
+  ) : void {
+    if (!this.config.isEnable) return;
+    if (viewport.zoom < this.config.minZoom) return;
+
+    const {width, height} = canvasSize;
+    const {offset, zoom} = viewport;
+
+    ctx.save();
+    ctx.strokeStyle = this.config.color;
+    ctx.lineWidth = this.config.width;
+    ctx.globalAlpha = this.config.opacity;
+
+    ctx.beginPath();
+
+    for (let x = 0; x < width; x += this.config.size) {
+      const sx = Math.round(offset.x + x * zoom) + 0.5
+      ctx.moveTo(sx, offset.y)
+      ctx.lineTo(sx, offset.y + height * zoom)
+    }
+
+    for (let y = 0; y < height; y += this.config.size) {
+      const sy = Math.round(offset.y + y * zoom) + 0.5
+      ctx.moveTo(offset.x, sy)
+      ctx.lineTo(offset.x + width * zoom, sy)
+    }
+
+    ctx.stroke()
+    ctx.restore()
+  }
+}
