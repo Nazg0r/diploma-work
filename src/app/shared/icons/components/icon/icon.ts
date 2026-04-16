cca774f43166eacc3158d5e5d158fd29d28d4184
Nazarii <gorchinshiy83@gmail.com>
2026-04-16 19:40:28 +0300

    feat: create icon component

---
+++ src/app/shared/icons/components/icon/icon.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  imports: [],
+  templateUrl: './icon.html',
+  styleUrl: './icon.scss',
+  host: {
+    'style.display': 'block',
+    '[style.width]': 'size()',
+    '[style.height]': 'size()',
+  }
+})
+export class Icon {
+  public readonly name = input.required<string>()
+  public readonly size = input<number>(32)
+}
