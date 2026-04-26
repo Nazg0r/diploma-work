905019fab5f5f4f82619a38ed82bf17108056f0d
Nazarii <gorchinshiy83@gmail.com>
2026-04-26 18:21:12 +0300

    upd: general updates and fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { PanelLayoutService } from './core/services/panel-layout.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideAppInitializer(() => {
+      inject(PanelLayoutService);
+    }),
+  ],
 };
--- src/app/shared/panel-system/directives/panel-drag.ts
+++ src/app/shared/panel-system/directives/panel-drag.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, inject, input, output } from '@angular/core';
-import { Size, Vector2 } from '../../../core/models/canvas.model';
+import { Vector2 } from '../../../core/models/canvas.model';
 
 @Directive({
   selector: '[appPanelDrag]',
@@ -13,7 +13,6 @@ export class PanelDrag {
   private isDragging: boolean = false;
   private startMousePos: Vector2 = { x: 0, y: 0 };
   private fixedStartPosition: Vector2 = { x: 0, y: 0 };
-  private fixedSize: Size = { width: 0, height: 0 };
 
   constructor() {
     this.host.nativeElement.addEventListener('pointerdown', this.onPointerDown);
@@ -26,11 +25,8 @@ export class PanelDrag {
     this.isDragging = true;
     this.startMousePos = { x: e.clientX, y: e.clientY };
     this.fixedStartPosition = this.startPos();
-    this.host.nativeElement.classList.add('panel-header__drag--active')
+    this.host.nativeElement.classList.add('panel-header__drag--active');
     this.host.nativeElement.setPointerCapture(e.pointerId);
-    ;
-    const header = this.host.nativeElement.parentElement as HTMLElement;
-    this.fixedSize = header.getBoundingClientRect();
 
     this.host.nativeElement.addEventListener('pointermove', this.onPointerMove);
     this.host.nativeElement.addEventListener('pointerup', this.onPointerUp);
@@ -38,11 +34,11 @@ export class PanelDrag {
 
   private readonly onPointerMove = (e: PointerEvent) => {
     if (!this.isDragging) return;
-    const raw = {
+    const newPos = {
       x: this.fixedStartPosition.x + e.clientX - this.startMousePos.x,
       y: this.fixedStartPosition.y + e.clientY - this.startMousePos.y,
     };
-    this.dragMove.emit(this.clamp(raw));
+    this.dragMove.emit(newPos);
   };
 
   private readonly onPointerUp = (e: PointerEvent) => {
@@ -51,14 +47,4 @@ export class PanelDrag {
     this.host.nativeElement.removeEventListener('pointermove', this.onPointerMove);
     this.host.nativeElement.removeEventListener('pointerup', this.onPointerUp);
   };
-
-  private clamp(pos: Vector2): Vector2 {
-    const vw = window.innerWidth;
-    const vh = window.innerHeight;
-
-    return {
-      x: Math.min(vw - this.fixedSize.width, Math.max(0, pos.x)),
-      y: Math.min(vh - this.fixedSize.height, Math.max(0, pos.y)),
-    };
-  }
 }
