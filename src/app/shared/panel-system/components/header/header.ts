44be2eb3bed8b602e5a9fb8534f2793243f1f583
Nazarii <gorchinshiy83@gmail.com>
2026-04-28 18:06:28 +0300

    feat: include menu overlay to header component

--- src/app/shared/panel-system/components/header/header.ts
+++ src/app/shared/panel-system/components/header/header.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { Icon } from '../../../icons/components/icon/icon';
+import { Component, inject } from '@angular/core';
 import { LG_ICON_SIZE } from '../../../../core/constants/size.constants';
+import { MenuStore } from '../../../../core/stores/menu/menu.store';
+import { Icon } from '../../../icons/components/icon/icon';
+import { MenuOverlay } from '../../../menu/components/menu-overlay/menu-overlay';
 
 @Component({
   selector: 'app-header',
-  imports: [Icon],
+  imports: [Icon, MenuOverlay],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   protected readonly LG_ICON_SIZE = LG_ICON_SIZE;
+
+  protected readonly store = inject(MenuStore);
+
+  protected onMenuToggle(event: PointerEvent) {
+    event.stopPropagation();
+    if (this.store.isOpen()) {
+      this.store.close();
+      return;
+    }
+    this.store.open();
+  }
 }
