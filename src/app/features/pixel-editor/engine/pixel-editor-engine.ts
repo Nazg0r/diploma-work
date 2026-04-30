38366a7d2d771fe6323b7693ce626254c1104c45
Nazarii <gorchinshiy83@gmail.com>
2026-04-30 21:34:57 +0300

    feat: include layer renderer to pixel editor engine

--- src/app/features/pixel-editor/engine/pixel-editor-engine.ts
+++ src/app/features/pixel-editor/engine/pixel-editor-engine.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@angular/core';
+import { AfterViewInit, effect, inject, Injectable } from '@angular/core';
+import { SpriteLayerStore } from '../../../core/stores/layers/sprite-layer.store';
 import { CanvasEngine } from './canvas-engine';
 import { Checkerboard } from './checkerboard';
 import { Grid, GridConfig } from './grid';
+import { PixelLayersRenderer } from './layer-renderer/pixel-layers-renderer';
 
 @Injectable()
-export class PixelEditorEngine extends CanvasEngine {
+export class PixelEditorEngine extends CanvasEngine implements AfterViewInit {
+  private readonly layerStore = inject(SpriteLayerStore);
+  private readonly layerRenderer = new PixelLayersRenderer();
   private readonly checkerboard = new Checkerboard();
   private readonly grid = new Grid();
+
+  ngAfterViewInit(): void {
+    effect(() => {
+      this.layerStore.layers();
+      this.layerStore.rootChildren();
+      this.markDirty();
+    });
+  }
 
   public override render(): void {
     const { ctx, canvas, viewport, canvasSize } = this;
@@ -21,6 +33,8 @@ export class PixelEditorEngine extends CanvasEngine {
     const sh = canvasSize.height * viewport.zoom;
 
     this.checkerboard.render(ctx, sx, sy, sw, sh, viewport.zoom);
+    const orderedLayers = this.layerStore.layersInRenderOrder();
+    this.layerRenderer.render(ctx, orderedLayers, viewport);
     this.grid.render(ctx, viewport, canvasSize);
 
     ctx.imageSmoothingEnabled = false;
