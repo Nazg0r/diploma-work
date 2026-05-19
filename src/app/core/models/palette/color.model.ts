f4254dbbc6e43365c6bb389a932d21237b479629
Nazarii <gorchinshiy83@gmail.com>
2026-05-19 18:24:55 +0300

    feat: extend color model

--- src/app/core/models/palette/color.model.ts
+++ src/app/core/models/palette/color.model.ts
@@ -3,7 +3,22 @@ export interface RGB {
   g: number;
   b: number;
 }
-
 export interface RGBA extends RGB {
+  a: number;
+}
+export interface HSL {
+  h: number;
+  s: number;
+  l: number;
+}
+export interface HSLA extends HSL {
+  a: number;
+}
+export interface HSV {
+  h: number;
+  s: number;
+  v: number;
+}
+export interface HSVA extends HSV {
   a: number;
 }
